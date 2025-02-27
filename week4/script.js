60fec11467780daa5824a1bff373817cc0ab2b65
Drilon Hyseni <drilon.hyseni02@gmail.com>
2025-02-27 09:47:04 +0000

    Week 4

---
+++ week4/script.js
@@ -0,0 +1,21 @@
+function print5() {
+  alert("1");
+  alert("2");
+  alert("3");
+  alert("4");
+  alert("5");
+}
+
+function print5InConsole() {
+  console.log("1");
+  console.log("2");
+  console.log("3");
+  console.log("4");
+  console.log("5");
+}
+
+//print5();
+//print5();
+
+print5InConsole();
+print5InConsole();
